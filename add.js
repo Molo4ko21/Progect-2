e5405561f56ddeb05da2337f83705bcc765892df
Nikolay <jokkey81@gmail.com>
2021-11-23 16:58:59 +0500

    fitst practik

--- add.js
+++ add.js
@@ -1,1 +1,18 @@
- 
+ const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actots: {},
+     genres: [],
+     privat: false
+ };
+
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На солько оцениваете его?', ''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На солько оцениваете его?', '');
+
+personalMovieDB.movies[a] = b;       
+personalMovieDB.movies[c] = d;     
+
+console.log(personalMovieDB)
